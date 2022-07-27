23d742f055da2da49de698183e188cd528c11853
Maham Khan <mahamimranamjad@gmail.com>
2022-07-27 15:05:48 -0700

    axios error

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { useEffect } from 'react'; 
+import axios from 'axios'
+ 
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import  Destinations from './components/Destinations';
 import  Home from './components/Home';
-import  Wishlist from './components/Wishlist';
-// import axios from 'axios'
+// import  Wishlist from './components/Wishlist';
+
+
 import Navbar from './components/Nav';
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
+
+
+const BASE_URL = 'http://localhost:3001/api'
+
+  useEffect(() => {
+    async function getDestinations() {
+      const res = await axios.get(BASE_URL)
+      console.log(res)
+    }
+    getDestinations()
+  }, [])
+
+
+
+
+
   return (
     <div className="App">
       {/* <Route path="/" element={ <Home /> } />
