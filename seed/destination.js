7dcf5e07c82c65c71e7d1eeeb3aa82ba76597c4c
Maham Khan <mahamimranamjad@gmail.com>
2022-07-28 11:03:08 -0700

    changed date back

--- seed/destination.js
+++ seed/destination.js
@@ -20,8 +20,8 @@ const addDestination = async () => {
         
         city: 'Santorini',
         country: 'Greece',
-        departure: 05/07/2020,
-        returndate: 05/19/2020,
+        departure: '05/07/2020',
+        returndate: '05/19/2020',
         people: 3,
         image: 'https://handluggageonly.co.uk/wp-content/uploads/2015/05/Hand-Luggage-Only-7.jpg' ,
         wishlist_id: wishlists[1]._id
@@ -30,8 +30,8 @@ const addDestination = async () => {
      
       city: 'Rome',
       country: 'Italy',
-      departure: 06/15/2020,
-      returndate: 06/23/2020,
+      departure: '06/15/2020',
+      returndate: '06/23/2020',
       people: 6,
       image: 'https://www.fodors.com/wp-content/uploads/2018/10/HERO_UltimateRome_Hero_shutterstock789412159.jpg',
       wishlist_id: wishlists[0]._id
@@ -40,8 +40,8 @@ const addDestination = async () => {
       
       city: 'Bern',
       country: 'Switzerland',
-      departure: 08/12/2021,
-      returndate: 08/20/2021,
+      departure: '08/12/2021',
+      returndate: '08/20/2021',
       people: 3,
       image: 'https://img.theculturetrip.com/450x/smart/wp-content/uploads/2021/02/k26rr6-e1617362650904.jpg',
       wishlist_id: wishlists[1]._id
@@ -50,8 +50,8 @@ const addDestination = async () => {
       
       city: 'Kobe',
       country: 'Japan',
-      departure: 10/10/2018,
-      returndate: 10/21/2018,
+      departure: '10/10/2018',
+      returndate: '10/21/2018',
       people: 7,
       image: 'https://www.worldtravelguide.net/wp-content/uploads/2017/03/shu-Japan-Nachisan-Seiganto_189118511-1440x823-1.jpg',
       wishlist_id: wishlists[1]._id
@@ -60,8 +60,8 @@ const addDestination = async () => {
      
       city: 'Banff',
       country: 'Canada',
-      departure: 06/04/2018,
-      returndate: 06/22/2018,
+      departure: '06/04/2018',
+      returndate: '06/22/2018',
       people: 9,
       image: 'https://www.banfflakelouise.com/sites/default/files/styles/l_1600_natural/public/kimsreizen-instagram-2132-ig-17954350378760829.jpg?itok=5_84dbq8',
       wishlist_id:wishlists[0]._id
